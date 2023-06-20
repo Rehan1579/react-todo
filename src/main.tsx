bfc4bd2e60d50719cde09ef9b381849de9c6ed0e
Rehan Hassan <rehan1579@gmail.com>
2023-06-20 18:26:19 +0500

    render NewApp (Todo)

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { NewApp } from './NewApp'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NewApp />
   </React.StrictMode>,
 )
