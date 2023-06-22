c9b85f5d47a22fe4cb523087933f495a97eab36e
Rehan Hassan <rehan1579@gmail.com>
2023-06-22 17:24:53 +0500

    break code into smaller components

--- src/NewApp.tsx
+++ src/NewApp.tsx
@@ -1,204 +1,76 @@
-import { FormEvent, Fragment, useState } from "react";
-
-
-
-interface ITodo {
-    id: string;
-    title: string;
-    complete: boolean;
-    editing: boolean;
-}
+import { useRef, useState } from "react";
+import { TodoService } from "./services/todo-service";
+import { ITodo } from "./data-models/ITodo";
+import { TodoList } from "./components/todo-list/TodoList";
+import { TodoForm } from "./components/todo-form/TodoForm";
 
 
 
 export function NewApp() {
 
-    const [editOn, setEditOn] = useState<boolean>(false);
-    const [title, setTitle] = useState<string>("");
-    const [todos, setTodos] = useState<ITodo[]>([
-        {id: "1", title: "Task 1", complete: false, editing: false},
-        {id: "2", title: "Task 2", complete: false, editing: false},
-        {id: "3", title: "Task 3", complete: false, editing: false},
-    ]);
+    const [todos, setTodos] = useState<ITodo[]>([]);
+    
+    const todoFormRef = useRef(null);
 
 
 
-    function submitForm(e: FormEvent<HTMLFormElement>): void {
-
-        e.preventDefault();
-        if(editOn)
-        {
-            return;
-        }
-
-
-        let newTodo: ITodo = {
-            id: crypto.randomUUID(),
-            title: title,
-            complete: false,
-            editing: false
-        };
-
-
-        addTodo(newTodo);
-        clearInput();
-    }
-
-
-
-    function addTodo(item: ITodo): void {
-        let updatedTodos = [...todos, item];
-        setTodos(updatedTodos);
-    }
-
-
-
-    function clearInput(): void {
-        setTitle("");
-    }
-
-
-
-    function removeTodo(id: string): void {
-
-        let canDelete = confirm("Are you sure you want to remove the todo?");
-        if(!canDelete)
-        {
-            return;
-        }
-
-
-        let updatedTodos = todos.filter((item) => item.id !== id);
-        setTodos(updatedTodos);
-    }
-
-
-
-    function editTodo(id: string, edit: boolean): void {
-
-        let updatedTodos = todos.map((item) => {
-
-            if(item.id === id)
-            {
-                if(edit)
-                {
-                    setTitle(item.title);  
-                }
-
-
-                return {
-                    ...item,
-                    editing: edit
-                }
-            }
-
-
-            return item;
-        });
-
+    function removeTodo(id: string) {
         
-        setEditOn(edit);
-        console.log(updatedTodos);
+        const canDelete = confirm("Are you sure you want to remove the todo?");
+        if (!canDelete)
+        {
+            return;
+        }
+
+
+        TodoService.removeTodo(id);
+        refreshTodo();
+    }
+
+
+
+    function refreshTodo() {
+        
+        const updatedTodos = TodoService.getTodos();
         setTodos(updatedTodos);
     }
 
 
 
-    function cancelEditing(id: string) {
-        clearInput();
-        editTodo(id, false);
+    function addTodo(payload: ITodo): void {
+        
+        TodoService.addTodo(payload);
+        refreshTodo();
     }
 
 
 
-    function saveTodo(id: string, title: string) {
-        updateTodo(id, "title", title);
+    function saveTodo(item: ITodo): void {
+        
+        addTodo(item);
 
-        // clearInput();
-        // setEditOn(false);
 
-        cancelEditing(id);
+        if(todoFormRef.current)
+        {
+            const form = todoFormRef.current as any;
+            form.resetForm();
+        }
     }
 
 
 
-    function toggleTodo(id: string, completed:boolean) {
-        updateTodo(id, "complete", completed);
-        cancelEditing(id);
-    }
-
-
-
-    function updateTodo(id:string, key:string, value: any): void {
-
-        let updatedTodos = todos.map((item) => {
-
-            if(item.id === id)
-            {
-                return {
-                    ...item,
-                    editing: false,
-                    [key]: value
-                }
-            }
-
-
-            return item;
-        });
-
-
-        setTodos(updatedTodos);
+    function toggleTodo(id:string, completed:boolean) {
+        
+        TodoService.toggleTodo(id, completed);
+        refreshTodo();
     }
 
 
 
     return (
         <div>
-            <form onSubmit={submitForm}>
-                <label htmlFor="title">Title</label>
-                <input 
-                    type="text"
-                    id="title"  
-                    required
-                    value={title} 
-                    onChange={e => setTitle(e.target.value)}
-                ></input>
-                
-                {editOn ? null : <button type="submit">Add</button>}
-            </form>
-            <div>
-                <ol>
-                    {todos.map((item) => {
-                        return (
-                            <li
-                                key={item.id}
-                                style={{textDecoration: item.complete ? 'line-through' : 'none'}}
-                            >
-                                
-                                <label>
-                                    <input type="checkbox" checked={item.complete} onChange={(e) => toggleTodo(item.id, e.target.checked)}></input>
-                                    {item.title}
-                                </label>
-
-
-                                {
-                                    item.editing
-                                        ? 
-                                            <>
-                                                <button onClick={() => saveTodo(item.id, title)}>Save</button>
-                                                <button onClick={() => cancelEditing(item.id)}>Cancel</button>
-                                            </>
-                                        :
-                                            <Fragment key={item.id}>
-                                                <button onClick={() => editTodo(item.id, true)}>Edit</button>
-                                                <button onClick={() => removeTodo(item.id)}>Remove</button>
-                                            </Fragment>
-                                }
-                            </li>
-                        ) 
-                    })}
-                </ol> 
-            </div>
+            <TodoForm saveTodo={saveTodo} ref={todoFormRef} />
+            <TodoList todoItems={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
         </div>
     );
 }
